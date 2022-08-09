7dc51828bb6f7f73fb4baced126537f0337ea760
tj-tieso <tj.tieso@gmail.com>
2022-08-09 14:56:56 -0500

    reducer tests

---
+++ client/src/utils/actions.js
@@ -0,0 +1,3 @@
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS"; // used by the ProductList component.
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
